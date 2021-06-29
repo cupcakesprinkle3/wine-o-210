df4dffd9ce7f2f2b3683acffaad7f7c8aea7e2e5
Emcee <mjchambersjr@gmail.com>
2021-06-28 19:53:23 -0700

    Fixed Vote on home, Style changes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -16,7 +16,7 @@ router.get('/', (req, res) => {
       'price', 
       'notes', 
       'created_at',
-      // [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE wine.id = vote.wine_id)'), 'vote_count']
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE wine.id = vote.wine_id)'), 'vote_count']
     ],
     include: [
       {
